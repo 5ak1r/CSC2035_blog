4e5be6b995b1ac0b702447dc9c5d43733b1a0917
5ak1r <s.azimkar1@newcastle.ac.uk>
2023-09-26 17:45:50 +0100

    init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+from blog.views import blog_blueprint
+from users.views import users_blueprint
+from main.views import main_blueprint
+
+
+app.register_blueprint(blog_blueprint)
+app.register_blueprint(users_blueprint)
+app.register_blueprint(main_blueprint)
+
+
+if __name__ == '__main__':
+    app.run()
