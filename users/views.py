e6bfb530c07933d5e01ae21dcec19b76308491f3
5ak1r <s.azimkar1@newcastle.ac.uk>
2023-10-03 16:52:26 +0100

    Register form stuff

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from users.forms import RegisterForm
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
@@ -16,3 +17,15 @@ def login():
 @users_blueprint.route('/account')
 def account():
     return render_template('users/account.html')
+
+
+@ users_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        print(form.get('username'))
+        print(form.get('password'))
+        return redirect(url_for('users.login'))
+
+    return render_template('users/register.html', form=form)
